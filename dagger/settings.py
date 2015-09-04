a1f132cdbd3d58a21a341bf8dbff03a375da2cd7
Marc de Falco <marc@de-falco.fr>
2015-09-04 08:55:40 +0200

    Logging

--- dagger/settings.py
+++ dagger/settings.py
@@ -64,6 +64,49 @@ INSTALLED_APPS = (
     'handouts'
 )
 
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'verbose': {
+                'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+                },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+                },
+            },
+        'handlers': {
+            'null': {
+                'level':'DEBUG',
+                'class':'django.utils.log.NullHandler',
+                },
+            'console':{
+                'level':'DEBUG',
+                'class':'logging.StreamHandler',
+                'formatter': 'simple'
+                },
+            'mail_admins': {
+                'level': 'ERROR',
+                'class': 'django.utils.log.AdminEmailHandler',
+                }
+            },
+        'loggers': {
+            'django': {
+                'handlers':['null'],
+                'propagate': True,
+                'level':'INFO',
+                },
+            'django.request': {
+                'handlers': ['mail_admins'],
+                'level': 'ERROR',
+                'propagate': False,
+                },
+            'cprepa.custom': {
+                'handlers': ['console', 'mail_admins'],
+                'level': 'INFO',
+                }
+            }
+        }
 COMMENTS_APP = 'fluent_comments'
 
 MIDDLEWARE_CLASSES = (
